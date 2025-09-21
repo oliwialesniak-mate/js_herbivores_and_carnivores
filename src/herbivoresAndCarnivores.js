3631465ccd17fe6aca0a17bfa734dd5ab8f0cbfb
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-22 01:33:39 +0200

    Create classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,57 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    // Herbivore może mieć hidden
+    if (this instanceof Herbivore) {
+      this.hidden = false;
+    }
+
+    // Dodajemy instancję do tablicy żywych
+    Animal.alive.push(this);
+  }
+
+  checkAlive() {
+    if (this.health <= 0) {
+      const index = Animal.alive.indexOf(this);
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+  }
+
+  bite(prey) {
+    // działa tylko na Herbivore i jeśli nie jest ukryty
+    if (!(prey instanceof Herbivore)) return;
+    if (prey.hidden) return;
+
+    prey.health -= 50;
+    if (prey.health <= 0) {
+      prey.checkAlive();
+    }
+  }
 }
 
 module.exports = {
